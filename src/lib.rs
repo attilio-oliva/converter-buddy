37d139a8a0bbb351829544915ae15911084c0645
attilio-oliva <attilio.oliva@protonmail.com>
2022-06-22 17:14:17 +0200

    add basic features

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,11 @@
+#[cfg(feature = "converters")]
 pub mod converter;
+
+#[cfg(feature = "formats")]
 pub mod format;
+
+#[cfg(feature = "io")]
 pub mod io;
+
+#[cfg(feature = "decoders")]
 pub mod decoder;
